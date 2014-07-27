853bfa2c19b5060f1e3d424eb35511b00f7bb7de
fenggee007 <fenggee007@163.com>
2014-07-27 11:50:07 +0800

    second commit

--- TemperatureTrans.c
+++ TemperatureTrans.c
@@ -5,13 +5,13 @@
 
 int main(int argc, char* argv[])	{
 	if(argc ==  2 && IsDec(argv[1]))	{
-		printf("the temperature is %.2f℃\n", f2c(atof(argv[1])));		
+		printf("The temperature is %.2f℃\n", f2c(atof(argv[1])));		
 	}
 	else if(argc == 3 && IsDec(argv[2]))	{
 		if( strcmp(argv[1], "f2c")==0 )
-			printf("the temperature is %.2f℃\n", f2c(atof(argv[2])));
+			printf("The temperature is %.2f℃\n", f2c(atof(argv[2])));
 		else if( strcmp(argv[1], "c2f") ==0 )
-			printf("the temperature is %.2f°F\n", c2f(atof(argv[2])));
+			printf("The temperature is %.2f°F\n", c2f(atof(argv[2])));
 	}
 	else	{
 		printf("Usage:\n");
--- cf.c
+++ cf.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "cf.h"
 
 float   c2f(float   fc)	{
 	return (fc*9/5+32) ;
@@ -20,12 +21,12 @@ int	IsDec(char* cInput)	{
 	for( ; i<iLength; i++)	{
 		if(cInput[i] == '.')
 			iDotNum++;
-		else if(cInput[i]>='0' & cInput[i]<='9')
+		else if(cInput[i]>='0' && cInput[i]<='9')
 			iIntergerNum++;
 		else
 			iOther++;
 	}
-	if(iDotNum>=0 && iIntergerNum>0 && !iOther)
+	if(iDotNum>=0 && iDotNum<=1 && iIntergerNum>0 && !iOther)
 		return 1;
 	else
 		return 0;
